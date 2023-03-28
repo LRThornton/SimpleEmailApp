0a47d066a16b86fbe841b5b4eb9da0fb10cd52d2
LRThornton <shodge51@aol.com>
2023-03-28 11:30:35 -0400

    Add project files.

---
+++ Services/EmailService/EMailService.cs
@@ -0,0 +1,35 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MimeKit.Text;
+using System;
+
+namespace SimpleEmailApp.Services.EmailService{
+    public class EMailService: IEmailService
+
+    {
+        private readonly IConfiguration _config;
+
+        public EMailService(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = "you have a new quote request to review";
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_config.GetSection("EmailUsername").Value, _config.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            
+        }
+    }
+}
